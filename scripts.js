2537514b7d7905f7d9df545682541b0f39afb9d5
Magda <magdaskonieckaa@gmail.com>
2024-01-06 04:27:46 +0100

    wiersze i okna modalne

---
+++ scripts.js
@@ -0,0 +1,228 @@
+var modalTexts = [
+    "Jutro chemia, o Boże!\n" +
+    "To już chyba nic nie pomoże.\n" +
+    "Pytam więc Grubego:\n" +
+    "\"Czy będzie coś trudnego?\"\n" +
+    "On na to się tylko przeciąga:\n" +
+    "\"Tobie coś da już tylko ściąga\".\n" +
+    "No to mówię jak obok leży Simba:\n" +
+    "\"Ty wiesz które to wiązania sigma?\"\n" +
+    "Patrzy się na mnie jak na idiotę:\n" +
+    "\"Co ty mi mówisz jakąś głupotę?\"\n" +
+    "Zirytowana idę więc do Padrugi:\n" +
+    "\"Żółty to chromian czy ten drugi?\"\n" +
+    "Ta to tylko się ze mnie śmieje:\n" +
+    "\"Ta Helena to stale głupieje\".\n" +
+    "\"A ty Płot pomożesz?\" - pytam z przekąsem,\n" +
+    "Ale ta to nawet nie ruszy wąsem.\n" +
+    "Zdesperowana idę więc do Orki:\n" +
+    "\"Dasz mi jakieś ostatnie korki?\"\n" +
+    "W trakcie lizania mi odpowiada:\n" +
+    "\"Oj biada Ci Helena, biada\"\n" +
+    "Wiec jak nie będę odwracać kota ogonem,\n" +
+    "To może się jutro dogadam z arkuszem"
+    ,
+
+    "Matura z matematyki:\n" +
+    "Na maturę z matematyki,\n" +
+    "uzyskałam niezłe wtyki:\n" +
+    "Jak już gorzej być nie może,\n" +
+    "delta zawsze ci pomoże"
+    ,
+
+    "Matura - język polski:\n" +
+    "Na maturze z polskiego,\n" +
+    "To ja nie chce Wokulskiego,\n" +
+    "Nie chce też Edka i Winstona,\n" +
+    "Zabierzcie Soplice lub Kreona\n" +
+    "Po co mi jakiś tam Tarrou,\n" +
+    "No i ten cały doktor Rieux\n" +
+    "Ktoś rozumie też Rodiona?\n" +
+    "Albo co robi Antygona?\n" +
+    "Najlepiej to wszystkich wywalcie,\n" +
+    "I od razu maturę odwołajcie"
+    ,
+
+    "Kolejny dzień, kolejne zajęcia:\n" +
+    "Co ja tu robię? Nie mam pojęcia.\n" +
+    "Dzień dopiero się zaczyna,\n" +
+    "a ja zaraz nie wytrzymam.\n" +
+    "Te wykłady - to jest zbrodnia,\n" +
+    "Byle dożyć do południa.\n\n" +
+    
+    "Kolejny tydzień, kolejne wejściówki,\n" +
+    "Jak je ominąć? Już nie ma wymówki.\n" +
+    "W mojej nauce - brak nawet wątku.\n" +
+    "Byle dożyć chociaż do piątku.\n\n" +
+    
+    "Kolejny miesiąc, kolejny egzamin:\n" +
+    "\"Ja nie płaczę - to oko łzawi\".\n" +
+    "Wciskam tak ludziom na każdym kroku.\n" +
+    "Byle dożyć do końca roku.\n\n" +
+    
+    "Sam sobie człowiek życie utrudnia:\n" +
+    "Byle do południa.\n" +
+    "Byle do grudnia.\n" +
+    "Byle skończyć studia"
+    ,
+
+    "Na moment się rozmarzam,\n" +
+    "wolne sobie wyobrażam.\n" +
+    "Przecież niedługo weekend, sobota,\n" +
+    "nagle myślę - ale wtopa!\n" +
+    "Sobotta ma przecież inne plany,\n" +
+    "egzamin jest zapowiedziany.\n\n" +
+    
+    "Atlas anatomii - cóż za rupieć!\n" +
+    "Mam to niby wszystko umieć?\n" +
+    "Nie mam siły znowu pytać,\n" +
+    "to spróbuję chociaż czytać.\n" +
+    "Sutura lambdoidea?\n" +
+    "Cartilago cirocoidea?\n" +
+    "To są kości czy zaklęcia?\n" +
+    "Ledwo trzymam się od klnięcia.\n\n" +
+    
+    "Może poszukam po angielsku,\n" +
+    "coś się dowiem o tym cielsku.\n" +
+    "Dobrze widzę? Jakiś Radius?\n" +
+    "Co to znaczy Metatarsus?\n\n" +
+    
+    "Nie no, nie mogę, nie rozumiem,\n" +
+    "po polsku, angielsku, łacinie - nie umiem.\n" +
+    "To przecież nie ma sensu, idę odkurzyć,\n" +
+    "zrobię wszystko, żeby się nie uczyć."
+    ,
+
+    "Cóż za cudowny poranek,\n" +
+    "gdy z kawą idę przed ganek.\n" +
+    "Słońce świeci, ptaki ćwierkają,\n" +
+    "liście pięknie kolor zmieniają.\n" +
+    "Jaki jest dzisiaj dzień tygodnia?\n" +
+    "Szalony piątek czy laba sobotnia?\n" +
+    "Nawet nie wiem, mam tydzień wolnego.\n" +
+    "Ten odpoczynek - coś pięknego!\n" +
+    "Każdy dzień jak weekend cudowny,\n" +
+    "obowiązek nauki - tylko umowny.\n" +
+    "Tyle miałam niby zrobić,\n" +
+    "wszystkie wykłady nadrobić.\n" +
+    "Na egzamin notatki napisać,\n" +
+    "od kogoś projekt spisać.\n" +
+    "Wyszło że nic nie zrobiłam,\n" +
+    "ale tym się nawet nie zdziwiłam.\n" +
+    "Bo tu przyznaję sobie rację,\n" +
+    "że ja to mam ciągle wakacje."
+    ,
+
+    "Leżę, odpoczywam,\n" +
+    "kocem się przykrywam.\n" +
+    "Biorę książkę, ciepłe kakao,\n" +
+    "By mi nic nie brakowało.\n" +
+    "Sprawdzam szybko powiadomienia,\n" +
+    "nagle serce mi zamiera.\n" +
+    "Jakby tego było mało,\n" +
+    "budzik mam na szóstą rano\n\n" +
+    
+    "Mam niby znowu wstać? Iść Tam?\n" +
+    "Nawet nie wiem czy ja zdam.\n" +
+    "Tej biologii - nie rozumiem,\n" +
+    "na wejściówki - nic nie umiem.\n" +
+    "Tego projektu - nie zrobiłam,\n" +
+    "z anatomii - się obijam.\n\n" +
+    
+    "Wzdycham, no i cóż począć?\n" +
+    "Że niby naukę rozpocząć?\n" +
+    "I gdy podręcznik tak otwieram,\n" +
+    "nagle sobie przypominam:\n" +
+    "Że ja przecież w moim planie,\n" +
+    "to mam teraz tylko spanie."
+    ,
+
+    "Na te wykłady,\n" +
+    "już nie ma rady.\n" +
+    "Ślęczę tutaj dwunastą godzinę,\n" +
+    "końca nawet nie widzę.\n" +
+    "Facet przynudza jak ciągle gada,\n" +
+    "a znowu zasnąć nie wypada,\n" +
+    "Nawet nie słucham, robię screeny,\n" +
+    "nie mam siły na krzywe miny.\n" +
+    "Cóż począć? Opuścić spotkanie?\n" +
+    "Już nawet wolę zrobić pranie.\n" +
+    "Jezus, zrobię dosłownie cokolwiek,\n" +
+    "Byle zatrzymać opadanie powiek.\n" +
+    "Dobra, skip to, umieram z nudów,\n" +
+    "rezygnuję i z wykładu i ze studiów."
+    ,
+
+    "Studia - tego nie robi się kotu.\n" +
+    "Bo co ma począć kot,\n" +
+    "na nudnym wykładzie*.\n\n" +
+    
+    "Dlatego chcę - choć na godzinę,\n" +
+    "jak kot - ominąć tę całą kpinę.\n" +
+    "Bo gdybym na studiach była kotem,\n" +
+    "pojawiałabym się - tylko przelotem.\n" +
+    "I tak przez całodniowe drzemki,\n" +
+    "Omijałabym wykładowe męki.\n\n" +
+    
+    "I nawet jeśli relacja z rektorem,\n" +
+    "byłaby trudna - jak psa z kotem.\n" +
+    "Wystarczyłoby otrzeć się o buty,\n" +
+    "żeby przestać drzeć z nim koty.\n\n" +
+    
+    "Żeby to wiedzieć - nie trzeba filozofa:\n" +
+    "Te studia to jest kot-astrofa.\n" +
+    "Bo jak ktoś mi biologię wykładał,\n" +
+    "to się czułam jak ten kot co napłakał"
+    ,
+
+    "Nie wiem jak na ciebie trafiłam,\n" +
+    "Pomogłeś gdy w życiu się zgubiłam.\n" +
+    "Upadłam, dotknęłam dna,\n" +
+    "nawet obiady brałam na dwa.\n" +
+    "Ledwo wiązałam koniec z końcem,\n" +
+    "gdy nagle zostałeś moim słońcem\n" +
+    "Stałeś się takim światełkiem w tunelu,\n" +
+    "gdy znów leżałam w łóżku bez celu.\n" +
+    "Nasze początki były spokojne, niewinne,\n" +
+    "ogłoszenia z książkami nie były nawet pilne.\n" +
+    "Nagle ruszyło wartko jak nawałnica,\n" +
+    "szybciej niż sam Robert Kubica.\n" +
+    "Klienci ciągle się do mnie dobijali\n" +
+    "Chcieli się dostać drzwiami, oknami.\n" +
+    "Z książek już nawet nic nie zostało,\n" +
+    "Chętnych do kupna i tak nie brakowało.\n" +
+    "Kiedyś -  w życiu mi się nie przelewało,\n" +
+    "Teraz przelewów na konto mam niemało.\n" +
+    "Wszystko, drogi Olx, dzięki Tobie,\n" +
+    "jesteś zbawieniem we własnej osobie.\n" +
+    "Wyprowadziłeś mnie na prostą - znalazłeś pracę,\n" +
+    "I wreszcie za kawę ja sama płacę."
+    ,
+    
+    "Nie ma teraz ważniejszej sprawy,\n" +
+    "niż ta, że nie piłam dzisiaj kawy.\n" +
+    "Marzy mi się z książką na tarasie,\n" +
+    "wypoczynek w pełnej klasie. \n" +
+    "I tak podziwiając trawę,\n" +
+    "chcę w spokoju sączyć kawę.\n" +
+    "Jeszcze z kotami wspólne leżenie\n" +
+    "Boże, no życie jak marzenie.\n" +
+    "Nie, niemożliwe - nagle się budzę!\n" +
+    "Okazuje się że tylko w kościele się nudzę"
+];
+
+function openModal(index) {
+    document.getElementById('myModal').style.display = 'block';
+    document.getElementById('modalText').innerText = modalTexts[index];
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+window.onclick = function (event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
